b0db72991d88f81b7d0fdfdaaf0bde687389878c
yuna <micha0215@tukorea.ac.kr>
2025-04-10 19:53:49 +0900

    백엔드 초기세팅

---
+++ src/main/java/techtrek/TechtrekApplication.java
@@ -0,0 +1,13 @@
+package techtrek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtrekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechtrekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/techtrek/TechtrekApplicationTests.java
@@ -0,0 +1,13 @@
+package techtrek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechtrekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
