624fa2f5f75937fb05b3abb6bf666f049e5e18b6
yuna <micha0215@tukorea.ac.kr>
2025-04-26 14:28:14 +0900

    [#1] chore: test용 어노테이션 추가

--- src/test/java/techtrek/TechtrekApplicationTests.java
+++ src/test/java/techtrek/TechtrekApplicationTests.java
@@ -2,7 +2,9 @@ package techtrek;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class TechtrekApplicationTests {
 
