51490ec4f84d4d244090c9ba8b18bff3fb00ac9a
yuna <micha0215@tukorea.ac.kr>
2025-05-24 02:20:31 +0900

    [#20] feat: 키워드 추가

--- src/main/java/techtrek/domain/basicQuestion/entity/status/QuestionCategory.java
+++ src/main/java/techtrek/domain/basicQuestion/entity/status/QuestionCategory.java
@@ -1,10 +1,22 @@
 package techtrek.domain.basicQuestion.entity.status;
 
 public enum QuestionCategory {
-    알고리즘,
-    자료구조,
-    데이터베이스,
-    네트워크,
-    컴퓨터구조,
-    운영체제
+    알고리즘(new String[]{"정렬", "탐색", "재귀", "백트래킹", "dp", "그리디", "이진탐색", "dfs", "bfs", "트리", "해시", "스택", "큐", "우선큐"}),
+    자료구조(new String[]{"배열", "리스트", "스택", "큐", "힙", "맵", "그래프", "bst", "avl", "트라이"}),
+    데이터베이스(new String[]{"인덱스", "트랜잭션", "정규화", "조인", "쿼리", "뷰", "프로시저", "격리수준", "mvcc", "교착상태", "nosql", "샤딩", "캐시전략", "메시지큐"}),
+    네트워크(new String[]{"tcp", "http", "dns", "cdn", "프록시", "nat", "포트", "소켓", "qos", "arp", "흐름제어", "혼잡제어", "윈도우", "트래픽", "스케일링", "msa", "이벤트", "api최적화"}),
+    컴퓨터구조(new String[]{"캐시", "파이프라인", "isa", "레지스터", "인터럽트", "사이클", "메모리", "cpu스케줄"}),
+    운영체제(new String[]{"프로세스", "스레드", "컨텍스트", "스케줄링", "동기화", "세마포어", "가상메모리", "페이징", "스와핑", "시스템콜", "교착상태", "장애복구"});
+
+    private final String[] keywords;
+
+    QuestionCategory(String[] keywords) {
+        this.keywords = keywords;
+    }
+
+    public String[] getKeywords() {
+        return keywords;
+    }
+
 }
+
--- src/main/java/techtrek/domain/sessionInfo/entity/status/EnterpriseName.java
+++ src/main/java/techtrek/domain/sessionInfo/entity/status/EnterpriseName.java
@@ -1,24 +1,53 @@
 package techtrek.domain.sessionInfo.entity.status;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum EnterpriseName {
-    삼성전자("전통 CS 기본기"),
-    쿠팡("대규모 트래픽, 인프라"),
-    네이버("웹 아키텍처 이해"),
-    카카오("설명력 중심 + 실무적 질문"),
-    배달의민족("실무 기반 시스템 설계, 최적화, 데이터 처리"),
-    당근마켓("최신 아키텍처 기반, 최적화, 데이터 처리"),
-    토스("Why 중심의 근본적 질문"),
-    넥슨( "게임 서버/동시성 특화");
+    삼성전자("전통 CS 기본기", new String[]{
+            "프로세스", "스레드", "캐시", "인덱스", "정규화", "정렬", "탐색", "tcp", "트랜잭션", "페이징", "스케줄링", "dfs", "bfs"
+    }),
+    쿠팡("대규모 트래픽, 인프라", new String[]{
+            "cdn", "트래픽", "캐시전략", "샤딩", "장애복구", "메시지큐", "스케일링", "http"
+    }),
+    네이버("웹 아키텍처 이해", new String[]{
+            "프록시", "http", "dns", "api최적화", "트랜잭션", "캐시전략", "msa", "프로세스"
+    }),
+    카카오("설명력 중심 + 실무적 질문", new String[]{
+            "스레드", "프로세스", "흐름제어", "정규화", "맵", "인터럽트", "해시", "정렬"
+    }),
+    배달의민족("실무 기반 시스템 설계, 최적화, 데이터 처리", new String[]{
+            "맵", "샤딩", "정합성", "트랜잭션", "cap", "api최적화", "큐"
+    }),
+    당근마켓("최신 아키텍처 기반, 최적화, 데이터 처리", new String[]{
+            "kafka", "이벤트", "msa", "비동기", "메시지큐", "pubsub", "비정규화"
+    }),
+    토스("Why 중심의 근본적 질문", new String[]{
+            "tcp", "격리수준", "인덱스", "해시", "정규화"
+    }),
+    넥슨("게임 서버/동시성 특화", new String[]{
+            "동기화", "락", "스레드", "큐", "트래픽", "패킷", "상태관리", "스케줄링"
+    }),
+    기타("전반적 CS 기반 면접", new String[]{
+            "운영체제", "자료구조", "정규화", "네트워크", "해시", "트리"
+    });
 
     private final String description;
+    private final String[] keywords;
 
-    EnterpriseName(String description) {
+    EnterpriseName(String description, String[] keywords) {
         this.description = description;
+        this.keywords = keywords;
     }
 
     public String getDescription() {
         return description;
     }
+
+    public List<String> getKeywords() {
+        return Arrays.asList(keywords);
+    }
 }
+
 
